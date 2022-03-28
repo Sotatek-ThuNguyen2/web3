0156ef78da3938246784175446dc67bc5656a93c
Sotatek-ThuNguyen2 <thu.nguyen2@sotatek.com>
2022-03-28 18:03:26 +0700

    update history

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { InjectedConnector } from "@web3-react/injected-connector";
-import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import { useWeb3React } from "@web3-react/core";
 import Web3 from "web3";
 import { useEffect, useState } from "react";
 import ERC20 from './ERC20.json';
-import {injected, walletConnectConnector} from './connector.js';
+import MASTERCHEF from './MASTER.json';
+import { injected, walletConnectConnector } from './connector.js';
 import { createClient } from 'urql';
-//import { fetchData } from "./subgraph";
+import BigNumber from 'bignumber.js';
+import { Multicall } from 'ethereum-multicall';
+import { Button, Table } from '@material-ui/core';
+import moment from 'moment';
 
-const APIURL = "https://api.thegraph.com/subgraphs/name/sotatek-thunguyen2/demo";
+const APIURL = "https://api.thegraph.com/subgraphs/name/sotatek-thunguyen2/masterchefcontract";
 
 const query = `
 {
-  depositEntities(first: 5, where: {address: "0x6B874665BF650aDFa4DAa52e7ba847F18C927290"}) {
+  historyEntities(where: {address: "0x6B874665BF650aDFa4DAa52e7ba847F18C927290"}) {
     id
     address
     amount
     time
-  }
-  withdrawEntities(first: 5, where: {address: "0x6B874665BF650aDFa4DAa52e7ba847F18C927290"}) {
-    id
-    address
-    amount
-    time
+    type
   }
 }
+
 `
 const client = createClient({
   url: APIURL
 })
 
-
 function App() {
-  const { account, chainId, connector, activate, library } = useWeb3React();
-  console.log('CHAINID:', chainId);
-  const [balance, setBlance] = useState(0);
-
+  const { account, activate, library } = useWeb3React();
+  const [wethBalance, setwethBlance] = useState(0);
+  const [deposited, setDeposited] = useState(0);
+  const [pendingReward, setPendingReward] = useState(0);
+  const [totalStake, setTotalStake] = useState(0);
+  const [history, setHistory] = useState([]);
+  const WethAddress = '0xc778417E063141139Fce010982780140Aa0cD5Ab';
+  const masterChefAddress = '0x9da687e88b0A807e57f1913bCD31D56c49C872c2'
   console.log(library);
   const connectInjectedConnector = () => {
     activate(injected);
@@ -47,64 +48,141 @@ function App() {
     activate(walletConnectConnector, undefined, true).catch(e => console.log('ee', e));
   };
 
-  const withdraw = async () => {
-    if (account && chainId && library) {
+  const getInfo = async () => {
+    try {
       const web3 = new Web3(library.provider);
-    } 
-  }
+      const multicall = new Multicall({ web3Instance: web3, tryAggregate: true });
+      const contractCallContext = [
+        {
+          reference: "weth",
+          contractAddress: WethAddress,
+          abi: ERC20,
+          calls: [
+            {
+              reference: "balance",
+              methodName: "balanceOf",
+              methodParameters: [account],
+            },
+            {
+              reference: "totalStake",
+              methodName: "totalSupply",
+              methodParameters: [],
+            },
+          ],
+        },
+        {
+          reference: "masterChef",
+          contractAddress: masterChefAddress,
+          abi: MASTERCHEF,
+          calls: [
+            {
+              reference: "userInfo",
+              methodName: "userInfo",
+              methodParameters: [account],
+            },
+            {
+              reference: "pendingDD2",
+              methodName: "pendingDD2",
+              methodParameters: [account],
+            },
+          ],
+        },
+      ];
+      const result = await multicall.call(contractCallContext);
+      console.log("result.results", result);
+      setwethBlance(
+        web3.utils.fromWei(
+          new BigNumber(
+            result.results.weth.callsReturnContext[0].returnValues[0].hex,
+            16
+          ).toFixed()
+        )
+      );
+      setTotalStake(
+        web3.utils.fromWei(
+          new BigNumber(
+            result.results.weth.callsReturnContext[1].returnValues[0].hex,
+            16
+          ).toFixed()
+        )
+      );
+      setDeposited(
+        web3.utils.fromWei(
+          new BigNumber(
+            result.results.masterChef.callsReturnContext[0].returnValues[0].hex,
+            16
+          ).toFixed()
+        )
+      );
+      setPendingReward(
+        web3.utils.fromWei(
+          new BigNumber(
+            result.results.masterChef.callsReturnContext[1].returnValues[0].hex,
+            16
+          ).toFixed()
+        )
+      )
+      console.log('result:', result);
 
-  const getBalance = async () => {
-    const web3 = new Web3(library.provider);
-    console.log(library.provider);
-    const wethContract = new web3.eth.Contract(ERC20, '0xc778417e063141139fce010982780140aa0cd5ab');
-    console.log(wethContract);
-    const balanceAccount = await wethContract.methods.balanceOf(account).call();
-    console.log(balanceAccount);
-    setBlance(web3.utils.fromWei(balanceAccount));
-  }
-
-  const getStaticInfo = async () => {
-    await getBalance();
-  };
-
-  const deposit = async () => {
-    const web3 = new Web3(library.provider);
-    console.log(library.provider);
-    const wethContract = new web3.eth.Contract(ERC20, '0xc778417e063141139fce010982780140aa0cd5ab');
-    await wethContract.methods.deposit().send({ value: web3.utils.toWei('0.1'), from: account });
-    console.log('DEPOSIT SUCCESS');
-  };
-
-  const [setData] = useState([]);
-  const fetchData = async () => {
-    const response = await client.query(query).toPromise();
-    console.log('response:', response)
-    setData(response.data);
+    } catch (error) {
+      console.log('error:', error)
+    }
   }
 
   useEffect(() => {
-    if(account){
-      getStaticInfo();
+    if (account) {
+
       fetchData()
     }
-  }, [account]);
-  return (
-    <div className="App">
-      <div style={{ marginTop: "4rem" }}>
-        {
-          account ? 
-          <> <h1>Account: {account} </h1> 
-              <h2>Balance: {balance}</h2>
-              <button onClick={deposit}>Deposit</button>
-          </> :
-          <> <button onClick={connectInjectedConnector}>Connect Metamask</button> 
-          <br/>
-            <button style={{ marginTop: '3rem' }} onClick={connectWalletConnectConnector}>Connect WalletConnect</button> 
-          </>
+  }, [account])
+  const fetchData = async () => {
+    const response = await client.query(query).toPromise();
+    console.log('response:', response)
+    setHistory(response.data.historyEntities);
+    console.log(history)
+  }
+  useEffect(() => {
+    if (account) {
+      getInfo();
+    }
 
-        }
-      </div>
+  }, [account, library])
+
+  return (
+
+    <div className="App">
+      <h1>Web3 React</h1>
+      {
+        account ? <>
+          <p>Wallet address: {account}</p>
+          <p>Balance : {wethBalance} WETH</p>
+          <p>Total Earn : { pendingReward} DD2</p>
+          <p>Your stake : {deposited} WETH</p>
+          <p>Total Stake: {totalStake}</p>
+          <Table striped bordered hover>
+            <thead>
+              <th>Action</th>
+              <th>Amount</th>
+              <th>Time</th>
+            </thead>
+            { history?.map((item) => (
+              <tr>
+                <td>{item.type}</td>
+                <td>
+                  {new BigNumber(item.amount)
+                    .dividedBy(10 ** 18)
+                    .toFormat()}
+                </td>
+                <td>{moment.unix(item.time).format('HH:MM  MM/DD/YYYY')}</td>
+              </tr>
+            ))}
+          </Table>
+        </> : <>
+          <Button style={{ marginRight: 10 }} variant="contained" color="primary" onClick={connectInjectedConnector}>Connect Metamask </Button>
+          <Button variant="contained" color="primary" onClick={connectWalletConnectConnector}>Connect WalletConnect </Button></>
+      }
     </div>
+
   );
 }
 
